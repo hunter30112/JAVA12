576d630bb4d36e4a93673267ee2c2673eb8fedd4
hunter30112 <92703484+hunter30112@users.noreply.github.com>
2022-01-09 18:14:02 +0800

    Update Class14.java

--- Class14.java
+++ Class14.java
@@ -3,9 +3,9 @@ package JAVA12;
 public class Class14 {
 
 	public static void main(String[] args) {
-		String str="Habit is second nature.";
-		System.out.println("String" +str);
-		System.out.println("sub-string=" +str.substring(16,22));
+		String str="Habit is second nature."; //讀入字串
+		System.out.println("String" +str); //印出字串
+		System.out.println("sub-string=" +str.substring(16,22)); //使用str.substring傳出指定段落的文字
 		
 
 	}
