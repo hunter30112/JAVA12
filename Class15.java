b27eb77f9d529d2e411b4f66b6592fc98b99120c
hunter30112 <92703484+hunter30112@users.noreply.github.com>
2022-01-09 18:16:03 +0800

    Update Class15.java

--- Class15.java
+++ Class15.java
@@ -5,10 +5,10 @@ package JAVA12;
 public class Class15 {
 
 	public static void main(String[] args) {
-		String str;
-		str = Float.toString(123.45f);
-		System.out.println(str);
-		System.out.println("length="+str.length());
+		String str; //定義空字串
+		str = Float.toString(123.45f); //輸入浮點數 並轉為字串
+		System.out.println(str); //印出文字
+		System.out.println("length="+str.length()); //使用str.length傳回字串長度
 
 	}
 
