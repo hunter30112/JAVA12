37e52cfe74805dd8437d5d48349dbebf6f38271b
hunter30112 <92703484+hunter30112@users.noreply.github.com>
2022-01-09 18:12:21 +0800

    Update Class13.java

--- Class13.java
+++ Class13.java
@@ -3,7 +3,7 @@ package JAVA12;
 public class Class13 {
 
 	public static void main(String[] args) {
-		System.out.println("round(5.38)="+Math.round(5.38));
+		System.out.println("round(5.38)="+Math.round(5.38)); //使用Math.round函數
 	}
 
 }
